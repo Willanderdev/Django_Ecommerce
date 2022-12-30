75cc2698c9fb0b6443f122efe14d3795b7b33c4b
willanderdev <willanderdev@gmail.com>
2022-12-29 23:43:04 -0300

    adcionando base de dados Postgresql

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -87,8 +87,12 @@ WSGI_APPLICATION = 'djangoecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Port-Ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'Tr4der2404',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
