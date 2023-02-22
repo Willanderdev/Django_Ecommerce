1c30e66a3d8538f39fb7a046bd784a83ddb636bc
willanderdev <willanderdev@gmail.com>
2023-02-22 11:37:09 -0300

    subindo banco de dados postgrsql do render

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -123,7 +123,7 @@ WSGI_APPLICATION = 'djangoecommerce.wsgi.application'
 # set DATABASE_URL = postgres://ecommerce_nnmm_user:xYRpA73nlbKvQBawRplpYkX7bDcvko1G@dpg-cfbrm5kgqg4aqevg6000-a/ecommerce_nnmm
 
 DATABASES = {
-    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 
 }
 
